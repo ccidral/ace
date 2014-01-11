33d07d36185bebb74e836fe66d6c4845ee840688
Célio Cidral Junior <ccidral@gmail.com>
2014-01-11 11:30:32 -0200

    ace_string: accept 'const char *' as parameter type instead of 'char *'

--- src/ace_string.c
+++ src/ace_string.c
@@ -13,7 +13,7 @@ char *ace_str_alloc (size_t length)
   return str;
 }
 
-char *ace_str_join_2 (char *s1, char *s2)
+char *ace_str_join_2 (const char *s1, const char *s2)
 {
   size_t length = strlen (s1) + strlen (s2) + 1;
   char *result = ace_str_alloc (length);
@@ -22,7 +22,7 @@ char *ace_str_join_2 (char *s1, char *s2)
   return result;
 }
 
-char *ace_str_join_3 (char *s1, char *s2, char *s3)
+char *ace_str_join_3 (const char *s1, const char *s2, const char *s3)
 {
   size_t length = strlen (s1) + strlen (s2) + strlen (s3) + 1;
   char *result = ace_str_alloc (length);
@@ -32,7 +32,7 @@ char *ace_str_join_3 (char *s1, char *s2, char *s3)
   return result;
 }
 
-bool ace_str_ends_with (char *str, char *expected_ending)
+bool ace_str_ends_with (const char *str, const char *expected_ending)
 {
   /* the code in this function was copied from:
      https://github.com/benbai123/C_Cplusplus_Practice/blob/master/C_StringProcessing/utils/startsWith_endsWith_indexOf_lastIndexOf.c
--- src/ace_string.h
+++ src/ace_string.h
@@ -3,8 +3,8 @@
 
 #include <stdbool.h>
 
-char *ace_str_join_2 (char *s1, char *s2);
-char *ace_str_join_3 (char *s1, char *s2, char *s3);
-bool ace_str_ends_with (char *str, char *expected_ending);
+char *ace_str_join_2 (const char *s1, const char *s2);
+char *ace_str_join_3 (const char *s1, const char *s2, const char *s3);
+bool ace_str_ends_with (const char *str, const char *expected_ending);
 
 #endif
